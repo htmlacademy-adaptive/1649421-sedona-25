4c6a678e277ca0a532433c07ac2958ca46a5baeb
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-05-05 23:39:25 +0400

    Pravka

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import squoosh from 'gulp-libsquoosh';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
@@ -39,7 +40,22 @@ const watcher = () => {
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('source/img'))
+}
+
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{png,jpg}')
+    .pipe(squoosh({
+      encodeOptions: {
+        webp: {},
+      },
+    }))
+    .pipe(gulp.dest('source/img'))
+}
 
 export default gulp.series(
-  styles, server, watcher
+  styles, optimizeImages, createWebp, server, watcher
 );
