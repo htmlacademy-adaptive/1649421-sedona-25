d4aab3929aeea7fca1d4051ca1e27372d89e38d6
Ilya-0208-pixe <77454591+Ilya-0208-pixe@users.noreply.github.com>
2022-05-23 22:29:25 +0400

    правки

--- build/js/script.js
+++
@@ -1,9 +0,0 @@
-const btnToggle = document.querySelector('.main-nav__toggle');
-const menu = document.querySelector('.main-nav');
-
-if (btnToggle) {
-  btnToggle.addEventListener('click', (item) => {
-    item.preventDefault();
-    menu.classList.toggle('main-nav--open');
-  });
-}
